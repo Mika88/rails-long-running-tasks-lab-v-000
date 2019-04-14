b6e504807648bdbafd3d12d9247956bd8e7dc0a3
Mika88 <mikaspivak@gmail.com>
2019-04-14 01:21:30 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :songs, only:[:index, :new, :show, :edit]
-  post 'songs', to: 'songs#upload'
+  post '/songs', to: 'songs#upload'
 end
